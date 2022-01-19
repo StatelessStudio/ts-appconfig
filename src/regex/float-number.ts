71cbbaa63c23ef7a66ed125e8b63c36b1aa53405
Drew Immerman <drewimmerman@gmail.com>
2022-01-19 00:53:00 -0500

    Float regex should accept negative values

--- src/regex/float-number.ts
+++ src/regex/float-number.ts
@@ -1,4 +1,4 @@
 /**
  * Regex for a floating point number, e.g. 123.45
  */
-export const floatNumberRegex = /^[0-9]+\.[0-9]+$/;
+export const floatNumberRegex = /^-?[0-9]+\.[0-9]+$/;
--- test/regex/float-number.spec.ts
+++ test/regex/float-number.spec.ts
@@ -9,6 +9,14 @@ describe('regex/float-number', () => {
 		expect(floatNumberRegex.test('123')).toBeFalse();
 	});
 
+	it('tests positive on signed float numbers', () => {
+		expect(floatNumberRegex.test('-123.45')).toBeTrue();
+	});
+
+	it('tests negative on signed integer numbers', () => {
+		expect(floatNumberRegex.test('-123')).toBeFalse();
+	});
+
 	it('tests negative on strings', () => {
 		expect(floatNumberRegex.test('hello')).toBeFalse();
 	});
